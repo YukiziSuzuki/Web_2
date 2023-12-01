359125131c3b35550fcaac8df12afef500fd0bf8
鈴木雪司 <61078145+YukiziSuzuki@users.noreply.github.com>
2023-12-01 11:24:08 +0900

    Add files via upload

---
+++ Web/test1.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head lang="ja">
+<meta http-equiv="Content-Type" content="text/html"; charset=utf8>
+<title>test1</title>
+</head>
+
+<body>
+<?php
+$com = 100;
+echo "COM:".$com;
+?>
+	
+</body>
+</html>
